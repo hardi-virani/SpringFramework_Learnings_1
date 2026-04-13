932a244f7151802dc0edd3925c5c29aaab678b07
Hardi <hardivirani2003@gmail.com>
2026-04-13 14:55:16 -0400

    Wrote comments at the top of main class, so see what was the steps.

--- src/main/java/com/star/App.java
+++ src/main/java/com/star/App.java
@@ -1,6 +1,9 @@
 // Check by running code at everystage to not have major bug in future.
-
 // 1. Since this is springframework, we are going to learn what going on behind the scene.
+
+// Tech work
+// Step 1 :  I want my spring to create this (obj) object for me. And we already saw how simple it is in springBoot, but NOW we are going to see what's happening behind the scene, basically in springFramework.
+// Step 2 : As we know we have IOC container where spring stored and managed the objects. We will make that first, So that spring can stored it.
 
 
 
