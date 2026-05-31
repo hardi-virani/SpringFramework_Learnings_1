63d8185b28a9a74a04a4af8ab7a397c7c8481342
Hardi <hardivirani2003@gmail.com>
2026-05-31 15:16:48 -0400

    Java based config

--- src/main/java/com/star/App.java
+++ src/main/java/com/star/App.java
@@ -14,8 +14,12 @@
 
 package com.star;
 
+import com.star.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import static org.springframework.context.annotation.AnnotationConfigApplicationContextExtensionsKt.AnnotationConfigApplicationContext;
 
 /**
  * Hello world!
@@ -25,6 +29,10 @@ public class App
 {
     public static void main( String[] args )
     {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
 //
 //        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
 //
--- src/main/java/com/star/config/AppConfig.java
+++ src/main/java/com/star/config/AppConfig.java
@@ -1,4 +1,15 @@
 package com.star.config;
 
-public class AppConfig {
+import com.star.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig  {
+
+    @Bean
+    public Desktop desktop() {
+        return new Desktop();
+    }
 }
